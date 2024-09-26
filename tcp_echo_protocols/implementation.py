a6b83034e0d45a0823982bdda21b53e6414d400e
MarcinDudek <dudek.v@gmail.com>
2024-09-26 20:29:10 +0200

    add OneConnectionAtTime implementation of ConnectionHandler prtocol

--- tcp_echo_protocols/implementation.py
+++ tcp_echo_protocols/implementation.py
@@ -1,3 +1,10 @@
+from socket import socket
+from protocols import MessageProcessor
+import logging
+
+logger = logging.getLogger(__name__)
+
+
 class EchoProcessor:
     def process_message(self, message: bytes) -> bytes:
         return message
@@ -14,3 +21,15 @@ class HardcodedConfig:
 
     def get_port(self) -> int:
         return 8080
+
+
+class OneConnectionAtTime:
+    def handle_connection(
+        self, conn: socket, addr, message_processor: MessageProcessor
+    ) -> None:
+        with conn:
+            logger.debug(f"Connected by {addr}")
+            data = conn.recv(1024)
+            if data:
+                conn.sendall(message_processor.process_message(message=data))
+            logger.info("Server closing after one echo")
