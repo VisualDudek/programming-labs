08c4442e8392fd9dc6aa6936a0400f1653fc3599
MarcinDudek <dudek.v@gmail.com>
2024-09-26 15:49:06 +0200

    add comment

--- tcp_echo_protocols/001-tcp_echo_server.py
+++ tcp_echo_protocols/001-tcp_echo_server.py
@@ -1,3 +1,4 @@
+# Init code for Protocol practice
 import socket
 import logging
 
@@ -5,13 +6,13 @@ import logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(
     level=logging.DEBUG,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    encoding='utf-8',
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    encoding="utf-8",
 )
 
 
-class Server():
-    def __init__(self, host='127.0.0.1', port=5000):
+class Server:
+    def __init__(self, host="127.0.0.1", port=5000):
         self.host = host
         self.port = port
 
@@ -19,11 +20,11 @@ class Server():
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((self.host, self.port))
             s.listen()
-            logger.info(f'Server listening on {self.host}:{self.port}')
-            
+            logger.info(f"Server listening on {self.host}:{self.port}")
+
             conn, addr = s.accept()
             with conn:
-                logger.debug(f'Connected by {addr}')
+                logger.debug(f"Connected by {addr}")
                 data = conn.recv(1024)
                 if data:
                     conn.sendall(data)
@@ -31,5 +32,5 @@ class Server():
 
 
 if __name__ == "__main__":
-    tcp_echo = Server(host='127.0.0.1', port=5000)
+    tcp_echo = Server(host="127.0.0.1", port=5000)
     tcp_echo.run()
