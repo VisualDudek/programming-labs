c4e9225ca0e3631726e1302d6b02127692c8a64c
MarcinDudek <dudek.v@gmail.com>
2024-09-26 20:30:08 +0200

    add ConnectionHandler and MessageProcessor interfaces

--- tcp_echo_protocols/004-tcp_echo_server.py
+++ tcp_echo_protocols/004-tcp_echo_server.py
@@ -2,8 +2,8 @@
 # ServerConfig, MessageProcessor, ConnectioHandler, ServerLifecycle
 import socket
 import logging
-from protocols import ServerConfig
-from implementation import HardcodedConfig
+from protocols import ServerConfig, ConnectionHandler, MessageProcessor
+from implementation import HardcodedConfig, OneConnectionAtTime, UpperCaseProcessor
 
 
 logger = logging.getLogger(__name__)
@@ -15,25 +15,32 @@ logging.basicConfig(
 
 
 class Server:
-    def __init__(self, config: ServerConfig):
+    def __init__(
+        self,
+        config: ServerConfig,
+        conn_handler: ConnectionHandler,
+        mess_processor: MessageProcessor,
+    ):
         self.host = config.get_host()
         self.port = config.get_port()
+        self.conn_handler = conn_handler
+        self.mess_processor = mess_processor
 
     def run(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             s.bind((self.host, self.port))
             s.listen()
             logger.info(f"Server listening on {self.host}:{self.port}")
 
             conn, addr = s.accept()
-            with conn:
-                logger.debug(f"Connected by {addr}")
-                data = conn.recv(1024)
-                if data:
-                    conn.sendall(data)
-                logger.info("Server closing after one echo")
+            self.conn_handler.handle_connection(conn, addr, self.mess_processor)
 
 
 if __name__ == "__main__":
-    server = Server(config=HardcodedConfig())
+    server = Server(
+        config=HardcodedConfig(),
+        conn_handler=OneConnectionAtTime(),
+        mess_processor=UpperCaseProcessor(),
+    )
     server.run()
